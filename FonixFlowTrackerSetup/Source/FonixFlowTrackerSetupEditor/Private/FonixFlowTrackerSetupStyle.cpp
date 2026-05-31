49583b0a66ccf69dc058bd4d9f9f804579bbf685
lcevelik <libor.cevelik@me.com>
2026-05-30 23:10:12 -0700

    Fix toolbar icon: use PNG instead of missing FSlateVectorImageBrush (not in UE5.6)

--- FonixFlowTrackerSetup/Source/FonixFlowTrackerSetupEditor/Private/FonixFlowTrackerSetupStyle.cpp
+++ FonixFlowTrackerSetup/Source/FonixFlowTrackerSetupEditor/Private/FonixFlowTrackerSetupStyle.cpp
@@ -8,17 +8,14 @@
 #include "Styling/CoreStyle.h"
 #include "SlateOptMacros.h"
 #include "Brushes/SlateImageBrush.h"
-#include "Brushes/SlateVectorImageBrush.h"
 #include "Misc/Paths.h"
 
 #undef IMAGE_BRUSH
 #undef BOX_BRUSH
 #undef BORDER_BRUSH
-#undef VECTOR_BRUSH
 #define IMAGE_BRUSH(RelativePath, ...) FSlateImageBrush(Style->RootToContentDir(RelativePath, TEXT(".png")), __VA_ARGS__)
 #define BOX_BRUSH(RelativePath, ...) FSlateBoxBrush(Style->RootToContentDir(RelativePath, TEXT(".png")), __VA_ARGS__)
 #define BORDER_BRUSH(RelativePath, ...) FSlateBorderBrush(Style->RootToContentDir(RelativePath, TEXT(".png")), __VA_ARGS__)
-#define VECTOR_BRUSH(RelativePath, ...) FSlateVectorImageBrush(Style->RootToContentDir(RelativePath, TEXT(".svg")), __VA_ARGS__)
 
 const FName FFonixFlowTrackerSetupStyle::StyleSetName("FonixFlowTrackerSetupStyle");
 TSharedPtr<FSlateStyleSet> FFonixFlowTrackerSetupStyle::StyleSet = nullptr;
@@ -67,20 +64,20 @@ TSharedRef<FSlateStyleSet> FFonixFlowTrackerSetupStyle::Create()
 	// Set content root to the plugin's Resources directory
 	Style->SetContentRoot(IPluginManager::Get().FindPlugin("FonixFlowTrackerSetup")->GetBaseDir() / TEXT("Resources"));
 
-	// Register SVG icon brushes
+	// Register PNG icon brushes
 	// Main plugin icon — blue "FF" square matching the panel header
-	Style->Set("FonixFlowTrackerSetup.Icon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
+	Style->Set("FonixFlowTrackerSetup.Icon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
 
 	// Toolbar icons (smaller versions)
-	Style->Set("FonixFlowTrackerSetup.WizardIcon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
-	Style->Set("FonixFlowTrackerSetup.AIChatIcon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
-	Style->Set("FonixFlowTrackerSetup.QuickSetupIcon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
+	Style->Set("FonixFlowTrackerSetup.WizardIcon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
+	Style->Set("FonixFlowTrackerSetup.AIChatIcon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
+	Style->Set("FonixFlowTrackerSetup.QuickSetupIcon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(20, 20)));
 
 	// Tab icons
-	Style->Set("FonixFlowTrackerSetup.TabIcon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(16, 16)));
+	Style->Set("FonixFlowTrackerSetup.TabIcon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(16, 16)));
 
 	// Large icon for panels
-	Style->Set("FonixFlowTrackerSetup.PanelIcon", new VECTOR_BRUSH("Icons/FonixFlowFF", FVector2D(40, 40)));
+	Style->Set("FonixFlowTrackerSetup.PanelIcon", new IMAGE_BRUSH("Icons/FonixFlowFF", FVector2D(40, 40)));
 
 	return Style;
 }
@@ -88,4 +85,3 @@ TSharedRef<FSlateStyleSet> FFonixFlowTrackerSetupStyle::Create()
 #undef IMAGE_BRUSH
 #undef BOX_BRUSH
 #undef BORDER_BRUSH
-#undef VECTOR_BRUSH
